d9ccd4cced95b699a82bc09d57180cb09baad08e
Abigail <abigail.edk@gmail.com>
2026-05-14 13:35:45 +0200

    Added venv requirements doc

--- main.py
+++ main.py
@@ -9,3 +9,37 @@ TODO:
             - Automate the TLE upadate via secure channel. 
     - Add method that can give the STM32 step/ramp reference inputs for when dish control loops and PID gains are tuned.
 '''
+
+# region ABOUT
+# endregion
+
+
+# region IMPORTS
+import os
+import sys
+import csv
+import time
+from datetime import datetime, timedelta, timezone
+from ESL_GroundStation.Prototype_Pi_Scripts.tracker import SatelliteTracker
+from ESL_GroundStation.Prototype_Pi_Scripts.uart import UARTComm
+# endregion
+
+# region PATHS
+HOME_DIR = os.path.expanduser("~")
+PROJECT_ROOT = os.path.join(HOME_DIR, "Desktop", "ESL_GroundStation")
+# endregion
+
+# region CLASSES
+
+# endregion
+
+# region FUNCTIONS
+# endregion
+
+# region MAIN
+def main():
+    print("Starting Ground Station Prototype...")
+
+
+if __name__ == "__main__": main()
+# endregion
